2f19aa8c108106229498c61c751dc28f98c18155
Elsammit <hhashimo25@gmail.com>
2020-08-22 09:54:57 +0900

    Release files

---
+++ precache-manifest.b10a0cd79ddd81edf2d946e593f6de93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a18374139126b1648c55a800c8f3e5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0df8543ed0b28262c0f1",
+    "url": "./static/css/main.d862e8a1.chunk.css"
+  },
+  {
+    "revision": "b905407e7a373cdffc76",
+    "url": "./static/js/2.86d9667a.chunk.js"
+  },
+  {
+    "revision": "392eb20231a966b4091f6354f05967df",
+    "url": "./static/js/2.86d9667a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df8543ed0b28262c0f1",
+    "url": "./static/js/main.26bcbfb8.chunk.js"
+  },
+  {
+    "revision": "0da4023613357375f157",
+    "url": "./static/js/runtime-main.7887763c.js"
+  }
+]);
